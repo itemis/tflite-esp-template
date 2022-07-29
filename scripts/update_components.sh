9708b361c5a27023291f5d036f139e067099007a
Rafael Tappe Maestro <rtappe@itemis.de>
2022-07-29 10:43:27 +0200

    updated component loader script

--- scripts/update_components.sh
+++ scripts/update_components.sh
@@ -2,9 +2,9 @@
 git submodule update --init --recursive # download submodules
 
 # TFMICRO and ESP-NN components
-# delete old components
-rm -r components/tfmicro
-rm -r components/esp-nn
+# delete old components if they exist
+rm -r components/tfmicro || true
+rm -r components/esp-nn || true
 # extract new components from submodule
 cp -r components/sources/tflite-micro-esp-examples/components/tflite-lib components/tfmicro
 cp -r components/sources/tflite-micro-esp-examples/components/esp-nn components/esp-nn
